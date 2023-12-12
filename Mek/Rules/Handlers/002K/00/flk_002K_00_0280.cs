d7610209cb4819966b1b8c3e2d9da54171121416
usilgtfoms <usilgtfoms@irkoms.ru>
2023-12-12 11:45:50 +0800

    flk_002K_00_0280

---
+++ Mek/Rules/Handlers/002K/00/flk_002K_00_0280.cs
@@ -0,0 +1,24 @@
+﻿using Mek.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mek.Rules.Handlers._002K._00
+{
+    public class flk_002K_00_0280 : HandlerFlk, ID, IR
+    {
+        public override void Handle(TreatmentCase request)
+        {
+            var docnum = request.Data?.Element("PACIENT")?.Element("DOCNUM")?.Value;
+            if (docnum == null)
+                return;
+
+            var docorg = request.Data?.Element("PACIENT")?.Element("DOCORG")?.Value;
+            if (docorg == null)
+                request.Result.Add(GetInfoOnError(request.Data, $"PACIENT/DOCNUM={docnum} DOCORG отсутсвует"));
+        }
+    }
+
+}
