e9f407b32d4f59541733544ff437284740e221b5
usilgtfoms <usilgtfoms@irkoms.ru>
2023-12-12 11:53:14 +0800

    flk_002K_00_0340

---
+++ Mek/Rules/Handlers/002K/00/flk_002K_00_0340.cs
@@ -0,0 +1,23 @@
+﻿using Mek.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mek.Rules.Handlers._002K._00
+{
+    public class flk_002K_00_0340 : HandlerFlk, IC, ID, IH, IR, IT, IX
+    {
+        public override void Handle(TreatmentCase request)
+        {
+            var dschet = request.Schet.Element("DSCHET")?.Value;
+            var date_z_2 = request.Data?.Element("Z_SL")?.Element("DATE_Z_2")?.Value;
+            if ((Convert.ToDateTime(date_z_2)) > Convert.ToDateTime(dschet))
+            {
+                request.Result.Add(GetInfoOnError(request.Data, $"DATE_Z_2 ={date_z_2}>DSCHET={dschet}"));
+            }
+        }
+    }
+
+}
