b809b93b3bfd074b3216e0a3346f46a0cb529597
usilgtfoms <usilgtfoms@irkoms.ru>
2023-12-13 09:02:10 +0800

    flk_002K_00_0390

---
+++ Mek/Rules/Handlers/002K/00/flk_002K_00_0390.cs
@@ -0,0 +1,27 @@
+﻿using Mek.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mek.Rules.Handlers._002K._00
+{
+    public class flk_002K_00_0390 : HandlerFlk, IC, ID, IH, IR
+    {
+        public override void Handle(TreatmentCase request)
+        {
+            var sl = request.Data?.Element("Z_SL")?.Elements("SL");
+            foreach (var s in sl)
+            {
+                var reab = s.Element("REAB")?.Value;
+                if (reab != null)
+                {
+                    var novor = request.Data?.Element("PACIENT")?.Element("NOVOR")?.Value;
+                    if (novor != "0")
+                        request.Result.Add(GetInfoOnError(request.Data, $"REAB={reab} при NOVOR={novor}"));
+                }
+            }
+        }
+    }
+}
