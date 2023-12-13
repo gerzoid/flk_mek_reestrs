93ca5f6b3e8526c2e11230d92ca64d3fff0a718b
usilgtfoms <usilgtfoms@irkoms.ru>
2023-12-13 08:59:23 +0800

    flk_002K_00_0350

---
+++ Mek/Rules/Handlers/002K/00/flk_002K_00_0350.cs
@@ -0,0 +1,27 @@
+﻿using Mek.Interfaces;
+using mek.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mek.Rules.Handlers._002K._00
+{
+    public class flk_002K_00_0350 : HandlerFlk, IX
+    {
+        public override void Handle(TreatmentCase request)
+        {
+            var rslt_d = Helper.GetValueAsInt(request.Data?.Element("Z_SL")?.Element("RSLT_D"));
+            var sl = request.Data?.Element("Z_SL")?.Elements("SL");
+            foreach (var s in sl)
+            {
+                var ds_onk = Helper.GetValueAsInt(s.Element("DS_ONK"));
+                if (ds_onk != null && ds_onk == 1 && rslt_d == 1)
+                {
+                    request.Result.Add(GetInfoOnError(request.Data, $"RSLT_D={rslt_d} DS_ONK={ds_onk}"));
+                }
+            }
+        }
+    }
+}
