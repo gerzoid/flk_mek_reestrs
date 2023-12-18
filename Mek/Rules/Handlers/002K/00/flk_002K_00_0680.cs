d85fc914f0118e2d4ff5de5a43fc61f9311b46ce
usilgtfoms <usilgtfoms@irkoms.ru>
2023-12-18 10:49:48 +0800

    flk_002K_00_0680

---
+++ Mek/Rules/Handlers/002K/00/flk_002K_00_0680.cs
@@ -0,0 +1,26 @@
+﻿using Mek.Interfaces;
+using mek.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mek.Rules.Handlers._002K._00
+{
+    public class flk_002K_00_0680 : HandlerFlk, IL, IR, IH, ID, IC, IT
+    {
+        public override void Handle(TreatmentCase request)
+        {
+            //request.Lm.has
+            var dr = Helper.GetValueAsDateTime(request.Lm?.Element("DR"));
+            if (dr == null)
+                return;
+            var data_z_1 = Helper.GetValueAsDateTime(request.Data?.Element("Z_SL")?.Element("DATE_Z_1"));
+
+            if (dr > data_z_1)
+                request.Result.Add(GetInfoOnError(request.Data, $"PERS/DR={dr}>ZGLV/DATA_Z_1={data_z_1}"));
+        }
+    }
+
+}
